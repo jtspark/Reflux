89422ab89585ef5307ddca2aee275605153f6cb2
Junji Terazawa <terazawa@jtspark.com>
2026-05-17 14:00:44 +0900

    fix: Retrieving the result song title
    Fixed a bug where the title of the previous song was sometimes retrieved.

--- Reflux/Program.cs
+++ Reflux/Program.cs
@@ -332,7 +332,7 @@ namespace Reflux
                             Console.WriteLine($"STATUS:{(newstate != GameState.playing ? " NOT" : "")} PLAYING");
                             if (newstate == GameState.resultScreen)
                             {
-                                Thread.Sleep(1000); /* Sleep to avoid race condition */
+                                Thread.Sleep(2000); /* Sleep to avoid race condition */
                                 var latestData = new PlayData();
                                 latestData.Fetch();
 
